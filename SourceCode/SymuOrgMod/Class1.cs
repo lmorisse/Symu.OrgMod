1a43d081cfbe5215af8bc12f250ec7f538e33d09
lmorisse <laurent.morisseau@gmail.com>
2020-10-06 17:22:24 +0200

    Initialise projects

---
+++ SourceCode/SymuOrgMod/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SymuOrgMod
+{
+    public class Class1
+    {
+        /// <summary>Determines whether the specified object is equal to the current object.</summary>
+        /// <param name="obj">The object to compare with the current object.</param>
+        /// <returns>true if the specified object  is equal to the current object; otherwise, false.</returns>
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+    }
+}
---
+++ SourceCode/SymuOrgModTests/Class1Tests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using SymuOrgMod;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SymuOrgMod.Tests
+{
+    [TestClass()]
+    public class Class1Tests
+    {
+        [TestMethod()]
+        public void EqualsTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
